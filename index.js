cda2d01e51a32211fc12439c2e8aa40676474cb7
MomandDadMac2 <MomandDadMac2@FrancesneysiMac.home>
2018-06-26 22:17:26 -0400

    tuesday edits

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 // Single state object
 var state = {
-	userSelectedSearchItem: ""
+  userSelectedSearchItem: "",
 };
+
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 function openModal() {
   document.getElementById('myModal').style.display = "block";
 }
-
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
 var slideIndex = 1;
-/*showSlides(slideIndex); */
+//showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -35,77 +32,149 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
- captionText.innerHTML = dots[slideIndex-1].alt;
+  //slides[slideIndex-1].style.display = "block";
+  //dots[slideIndex-1].className += " active";
+captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
+
 function renderVid(vidforModal) {
-  console.log(`made it to renderModal`);
   
   return `
     <div class="mySlides">
-    <iframe width="420" height="345" src="https://www.youtube.com/embed/${vidforModal.id.videoId}?controls=1"" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <iframe width="420" height="345" src="https://www.youtube.com/embed/${vidforModal.id.videoId}?controls=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
   `;
 }
 
-function renderTitleThumbDesc(titleThumbDesc) {
-  return `
-    <div class="col-3">
-      
-      <h3><span class="js-result-name">
-      <a href="${titleThumbDesc.snippet.title}" style="width:100%" target="_blank">${titleThumbDesc.snippet.title}</a>
-      </span></h3>
-      <span class="js-image"><p>
-      <img class="hover-shadow cursor" data-videoid="${titleThumbDesc.id.videoId}" src="${titleThumbDesc.snippet.thumbnails.medium.url}" style="width:100%" onclick="openModal();currentSlide(1)" ></p>
-      </span>
-      <span class="js-desc">
-      <p>${titleThumbDesc.snippet.description}</p>
-      </span>
-     
-    </div>
-  `;
-}
 
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-function getDataFromApi(callback) {
-  
+
+// Queries to the API
+function getAPIData(callback) {
   const query = {
     q: `${userSelectedSearchItem} in:name`,
     key: `AIzaSyDoLr1m73oBf7SHHiLQMEXg_8nhHUBWLYM`,
     part: 'snippet',
-    per_page: 'results',
+    maxResults: 8,
+    per_page: 4,
     videoID:'id'
   }
  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
 
-function displayYouTubeSearchData(data) {
-  console.log(data);
-  const titleThumbDesc = data.items.map((item, index) => renderTitleThumbDesc(item));
+function getAPIDataNextPage(token, callback) {
+  const query = {
+    q: `${userSelectedSearchItem} in:name`,
+    key: `AIzaSyDoLr1m73oBf7SHHiLQMEXg_8nhHUBWLYM`,
+    part: 'snippet',
+    maxResults: 8,
+    per_page: 4,
+    videoID:'id',
+    pageToken: token
+  }
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+  //renderAPIDataNextPage(data);
+}
+
+// Render the API data
+function renderAPIData(data) {
+    var nextToken = data.nextPageToken;
+    console.log('made it to displayYouTubeSearchData:' + nextToken);
+   const titleThumbDesc = data.items.map((item, index) => renderTitleThumbDesc(item));
+   const vidforModal = data.items.map((item, index) => renderVid(item));
+   $('#js-search-title').html(`<h2 style="text-align:center">The following are the Thinkful Tube results for :  ${userSelectedSearchItem}</h2>`);
+   $('.js-search-results').html(titleThumbDesc);
+   $('.js-modal-content').html(vidforModal);
+   nextButton(nextToken);
+}
+
+function renderTitleThumbDesc(titleThumbDesc) {
+  //console.log(titleThumbDesc);
+  return `
+    <div id="searchresults" class="col-3">
+      <h3><div class="js-result-name">
+      <a href="${titleThumbDesc.snippet.title}" style="width:100%" target="_blank">${titleThumbDesc.snippet.title}</a>
+      </div></h3>
+      <div class="js-image"><p>
+      <img class="hover-shadow cursor" data-videoid="${titleThumbDesc.id.videoId}" src="${titleThumbDesc.snippet.thumbnails.medium.url}" style="width:100%" onclick="openModal();currentSlide(1)" ></p>
+      </div>
+      <div class="js-desc">
+      <p>${titleThumbDesc.snippet.description}</p>
+      </div>
+    </div>
+  `;
+}
+function renderAPIDataNextPage(data){
+    let nextToken = data.nextPageToken;
+    console.log('nexttoken is:' + nextToken);
+    let titleThumbDesc = data.items.map((item, index) => replaceTitleThumbDesc(item));
+   // const vidforModal = data.items.map((item, index) => renderVid(item));
+   let parent = document.getElementById("js-search-results");
+   let child = document.getElementById("searchresults");
+   while (parent.hasChildNodes()) { 
+     console.log("this is the parent: " + parent.nodeName );
+     parent.firstChild.remove();
+   }
+  $('.js-search-results').html(titleThumbDesc);
+}
+
+
+function replaceTitleThumbDesc(titleThumbDesc) {
+  return `
+    <div class="col-3">
+      <h3><div class="js-result-name">
+      <a href="${titleThumbDesc.snippet.title}" style="width:100%" target="_blank">${titleThumbDesc.snippet.title}</a>
+      </div></h3>
+      <div class="js-image"><p>
+      <img class="hover-shadow cursor" data-videoid="${titleThumbDesc.id.videoId}" src="${titleThumbDesc.snippet.thumbnails.medium.url}" style="width:100%" onclick="openModal();currentSlide(1)" ></p>
+      </div>
+      <div class="js-desc">
+      <p>${titleThumbDesc.snippet.description}</p>
+      </div>
+    </div>
+  `;
+}
+    
+
+function renderNextPage(obj) {
+  const titleThumbDesc = data.items.map((item, index) => replaceTitleThumbDesc(item));
   const vidforModal = data.items.map((item, index) => renderVid(item));
-  $('#js-search-title').html(`<h2 style="text-align:center">The following are the Thinkful Tube results for :  ${userSelectedSearchItem}</h2>`);
+  console.log('about to render html to js-search-results');
   $('.js-search-results').html(titleThumbDesc);
   $('.js-modal-content').html(vidforModal);
-  
+   //nextButton(nextToken);
+}
+   
+/* Event listeners */
+function closeModal() {
+  $('.close').on('click', 'span', function (event){
+    console.log('made it to click');
+    $('mySlides').trigger('pause');
+    //document.getElementById('myModal').style.display = "none";
+  });
+}
+
+function nextButton(token){
+  //listen for button click, execute the code
+  $('#next').on('click', function(e) {
+    e.preventDefault();
+    getAPIDataNextPage(token,renderAPIDataNextPage);
+  });
 }
 
 function watchSubmit() {
-  $('.js-search-form').submit(event => {
-    event.preventDefault();
-    const queryTarget = $(event.currentTarget).find('.js-query');
+  $('.js-search-form').submit(e => {
+    e.preventDefault();
+    const queryTarget = $(e.currentTarget).find('.js-query');
     userSelectedSearchItem  = queryTarget.val();
     // clear out the input
     queryTarget.val("");
-    console.log('after queryTarget');
-    getDataFromApi(displayYouTubeSearchData);
-
-   
+    getAPIData(renderAPIData);
   });
 }
 
 
 watchSubmit();
+closeModal();
 
