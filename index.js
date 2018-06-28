2df2f61c2336982a36b7a05bda0034dbcc275027
MomandDadMac2 <MomandDadMac2@FrancesneysiMac.home>
2018-06-27 23:18:16 -0400

    show more data and show video in modal

--- index.js
+++ index.js
@@ -1,180 +1,113 @@
 // Single state object
 var state = {
-  userSelectedSearchItem: "",
+  userSelectedSearchTerm: '',
+  nextPageToken: ''
 };
 
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-var slideIndex = 1;
-//showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  //slides[slideIndex-1].style.display = "block";
-  //dots[slideIndex-1].className += " active";
-captionText.innerHTML = dots[slideIndex-1].alt;
-}
+const youtubeURL = 'https://www.googleapis.com/youtube/v3/search';
 
 
-function renderVid(vidforModal) {
-  
-  return `
-    <div class="mySlides">
-    <iframe width="420" height="345" src="https://www.youtube.com/embed/${vidforModal.id.videoId}?controls=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    </div>
-  `;
-}
-
-
-
-
-// Queries to the API
-function getAPIData(callback) {
-  const query = {
-    q: `${userSelectedSearchItem} in:name`,
-    key: `AIzaSyDoLr1m73oBf7SHHiLQMEXg_8nhHUBWLYM`,
-    part: 'snippet',
-    maxResults: 8,
-    per_page: 4,
-    videoID:'id'
-  }
- $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
-}
-
-function getAPIDataNextPage(token, callback) {
-  const query = {
-    q: `${userSelectedSearchItem} in:name`,
-    key: `AIzaSyDoLr1m73oBf7SHHiLQMEXg_8nhHUBWLYM`,
-    part: 'snippet',
-    maxResults: 8,
-    per_page: 4,
-    videoID:'id',
-    pageToken: token
-  }
-  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
-  //renderAPIDataNextPage(data);
-}
-
-// Render the API data
+// Render the API data to the DOM
 function renderAPIData(data) {
-    var nextToken = data.nextPageToken;
-    console.log('made it to displayYouTubeSearchData:' + nextToken);
-   const titleThumbDesc = data.items.map((item, index) => renderTitleThumbDesc(item));
-   const vidforModal = data.items.map((item, index) => renderVid(item));
-   $('#js-search-title').html(`<h2 style="text-align:center">The following are the Thinkful Tube results for :  ${userSelectedSearchItem}</h2>`);
-   $('.js-search-results').html(titleThumbDesc);
-   $('.js-modal-content').html(vidforModal);
-   nextButton(nextToken);
-}
-
-function renderTitleThumbDesc(titleThumbDesc) {
-  //console.log(titleThumbDesc);
-  return `
-    <div id="searchresults" class="col-3">
-      <h3><div class="js-result-name">
-      <a href="${titleThumbDesc.snippet.title}" style="width:100%" target="_blank">${titleThumbDesc.snippet.title}</a>
+  $('.dataItem').remove();
+  var indexNum = 0;
+  $.each(data, function(index, item) {
+    indexNum +=1;
+    $(`#js-search-results`).append(`
+      <div class='dataItem col-3'>
+      <h3><div class='js-result-name'>
+        <a href='${item.snippet.title}' style='width:100%' target='_blank'>${item.snippet.title}</a>
       </div></h3>
-      <div class="js-image"><p>
-      <img class="hover-shadow cursor" data-videoid="${titleThumbDesc.id.videoId}" src="${titleThumbDesc.snippet.thumbnails.medium.url}" style="width:100%" onclick="openModal();currentSlide(1)" ></p>
+      <div class='js-image'><p>
+      <img class='hover-shadow cursor' data-videoid='${item.id.videoId}' src='${item.snippet.thumbnails.medium.url}' ></p>
       </div>
-      <div class="js-desc">
-      <p>${titleThumbDesc.snippet.description}</p>
+      <div class='js-desc'>
+      <p>${item.snippet.description}</p>
       </div>
-    </div>
-  `;
-}
-function renderAPIDataNextPage(data){
-    let nextToken = data.nextPageToken;
-    console.log('nexttoken is:' + nextToken);
-    let titleThumbDesc = data.items.map((item, index) => replaceTitleThumbDesc(item));
-   // const vidforModal = data.items.map((item, index) => renderVid(item));
-   let parent = document.getElementById("js-search-results");
-   let child = document.getElementById("searchresults");
-   while (parent.hasChildNodes()) { 
-     console.log("this is the parent: " + parent.nodeName );
-     parent.firstChild.remove();
-   }
-  $('.js-search-results').html(titleThumbDesc);
-}
-
-
-function replaceTitleThumbDesc(titleThumbDesc) {
-  return `
-    <div class="col-3">
-      <h3><div class="js-result-name">
-      <a href="${titleThumbDesc.snippet.title}" style="width:100%" target="_blank">${titleThumbDesc.snippet.title}</a>
-      </div></h3>
-      <div class="js-image"><p>
-      <img class="hover-shadow cursor" data-videoid="${titleThumbDesc.id.videoId}" src="${titleThumbDesc.snippet.thumbnails.medium.url}" style="width:100%" onclick="openModal();currentSlide(1)" ></p>
-      </div>
-      <div class="js-desc">
-      <p>${titleThumbDesc.snippet.description}</p>
-      </div>
-    </div>
-  `;
-}
-    
-
-function renderNextPage(obj) {
-  const titleThumbDesc = data.items.map((item, index) => replaceTitleThumbDesc(item));
-  const vidforModal = data.items.map((item, index) => renderVid(item));
-  console.log('about to render html to js-search-results');
-  $('.js-search-results').html(titleThumbDesc);
-  $('.js-modal-content').html(vidforModal);
-   //nextButton(nextToken);
-}
-   
-/* Event listeners */
-function closeModal() {
-  $('.close').on('click', 'span', function (event){
-    console.log('made it to click');
-    $('mySlides').trigger('pause');
-    //document.getElementById('myModal').style.display = "none";
+      <div class="col-12 modal">
+        <!-- Modal content-->
+        <span class="close cursor">&times;</span>
+        <span id="modalLogo"><h1><i class="fas fa-kiwi-bird"></i>  MissyTube </h1></span>
+          <div class='myVideo' id='${indexNum}'>
+            <iframe data-videoIndex = ${index} src='https://www.youtube.com/embed/${item.id.videoId}?controls=1'></iframe>
+          </div>
+      </div>`
+    );
   });
 }
 
-function nextButton(token){
-  //listen for button click, execute the code
-  $('#next').on('click', function(e) {
-    e.preventDefault();
-    getAPIDataNextPage(token,renderAPIDataNextPage);
+    
+
+// Queries to the API, get the data
+function getAPIData(userSelectedSearchItem) {
+  const params = {
+    q: `${userSelectedSearchItem} in:name`,
+    key: `AIzaSyDoLr1m73oBf7SHHiLQMEXg_8nhHUBWLYM`,
+    part: 'snippet',
+    maxResults: 8,
+    videoID:'id'
+  }
+ $.getJSON(youtubeURL, params, function(data){
+   renderAPIData(data.items);
+   nextPageToken = data.nextPageToken;
+ });
+}
+
+function getAPIDataNextPage(userSelectedSearchTerm) {
+  const params = {
+    q: `${userSelectedSearchTerm} in:name`,
+    key: `AIzaSyDoLr1m73oBf7SHHiLQMEXg_8nhHUBWLYM`,
+    part: 'snippet',
+    maxResults: 8,
+    videoID:'id',
+    pageToken: nextPageToken
+  }
+  $.getJSON(youtubeURL, params, function(data){
+    renderAPIData(data.items);
+     nextPageToken = data.nextPageToken;
+  });
+}
+
+/* Event listeners */
+
+function closeModal() {
+  $('.close').on('click', 'span', function (event){
+    console.log('made it to click');
+    $('myvideos').trigger('pause');
+    //document.getElementById('myModal').style.display = 'none';
   });
 }
 
 function watchSubmit() {
-  $('.js-search-form').submit(e => {
-    e.preventDefault();
-    const queryTarget = $(e.currentTarget).find('.js-query');
-    userSelectedSearchItem  = queryTarget.val();
-    // clear out the input
-    queryTarget.val("");
-    getAPIData(renderAPIData);
+  $('.js-search-form').submit(function(event){
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    userSelectedSearchTerm  = queryTarget.val();
+    getAPIData(userSelectedSearchTerm);
+    $('#next').show();
+
+  });
+
+  $('#next').on('click', function(event){
+    event.preventDefault();
+    getAPIDataNextPage(userSelectedSearchTerm);
+  })
+
+  //open modal here
+  $('#js-search-results').on('click', '.js-image', function(event){
+    event.preventDefault();
+    $(event.currentTarget).closest(".dataItem").find(".modal").show();
+  });
+
+  //get the span element that closes the modal
+  $('.close').on('click', function(event){
+    //event.preventDefault();
+    //$(event.currentTarget).closest(".modal").hide();
+    console.log('got the close button');
+    //modal.style.display="none";
   });
 }
 
 
 watchSubmit();
-closeModal();
 
